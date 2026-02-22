94cfd82a6abee9cf29737e11a355f1a26b46bd60
David Fried <davidfried@Davids-MacBook-Pro.local>
2026-02-22 23:07:15 +0200

    Add Repository

--- env.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const envSchema = z.object({
-  PORT: z.coerce.number(),
-  DEBUG: z.coerce.boolean(),
-  API_KEY: z.string(),
-});
-
-export type Env = z.infer<typeof envSchema>;
